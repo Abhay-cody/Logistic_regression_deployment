6f1a3cf4f426b528a5ef5bed03fc6309c7fb2dc7
Abhay-cody <abhaykumargupta12312@gmail.com>
2026-07-04 14:23:12 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(df[['age']],df.bought_insurance,train_size=0.8)
 
-X_test
+# X_test
 
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()
